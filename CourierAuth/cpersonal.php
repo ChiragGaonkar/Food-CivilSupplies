e3c982e2a8f67eb8a34f031888a8f4ece82e8ab2
Akshad05 <95163463+Akshad05@users.noreply.github.com>
2021-11-30 15:03:27 +0530

    Updated
    Created courierboy detail display page

--- CourierAuth/cpersonal.php
+++ CourierAuth/cpersonal.php
@@ -1,43 +1,113 @@
 <?php
-include "../config.php";
 session_start();
 error_reporting(0);
 
 ?>
 
+<?php include 'courierboydetails.php' ?>
 <!doctype html>
 <html lang="en">
-
-<head>
+  <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="cdetailcontainer.css">
+    <title>Courier</title>
+  </head>
+  <body>
+       <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top" style="padding: 10px 30px 10px 30px">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="../home.php">
+                <!-- <img src="images/AdminLogo.png" style="width: 40px;" alt="Admin">
+                <img src="images/UserLogo.png" style="width: 40px;" alt="Admin">
+                <img src="images/DeliveryLogo.png" style="width: 40px;" alt="Admin"> -->
+                Food & Civil Supplies
+            </a>
 
-    <title>Hello, world!</title>
-</head>
+            <!-- Button which pops when window is minimized -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-<body>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav" style="margin-left: auto;">
+                    <!-- Home -->
+                    <li class="nav-item">
+                        <a class="nav-link" style="margin-right: 20px;" aria-current="page" href="../home.php">Orders</a>
+                    </li>
 
-    <h1>Personal Info</h1>
-    <?php echo "<h1>Welcome " . $_SESSION['fname'] . "</h1>"; ?>
-    <a href="clogout.php">Log me out</a>
+                    <!-- Contact Us -->
+                    <li class="nav-item">
+                        <a class="nav-link" style="margin-right: 10px;" href="#">Contact Us</a>
+                    </li>
+
+                    <!-- LogOut -->
+                    <li class="nav-item">
+                        <a class="nav-link" style="margin-right: 10px;" href="clogout.php">Log-Out</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- End of Navbar -->
+
+    <!-- container -->
+    <div class="container">
+    <div class="card text-center">
+  <div class="card-header" style="background-color: rgb(110 94 94 / 27%); border-bottom: rgb(51 43 43 / 9%);">
+  <b>YOUR DETAILS</b>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title"></h5>
+    <div class="subcontainer">
+        <p class="card-text"><strong>ID : </strong><?php echo $row['CID']; ?></p>
+        <p class="card-text"><strong>AADHAR NUMBER : </strong><?php echo $row['CAADHARNUM'];  ?></p>
+        <p class="card-text"><strong>FIRST NAME : </strong><?php echo $row['CFNAME']; ?></p>
+        <p class="card-text"><strong>LAST NAME : </strong><?php echo $row['CLNAME']; ?></p>
+        <p class="card-text"><strong>EMAIL ID : </strong><?php echo $row['CEMAIL']; ?></p>
+        <p class="card-text"><strong>DISTRICT : </strong><?php echo $row['CDISTRICT']; ?></p>
+    </div>
+    
+  </div>
+  <div class="card-footer text-muted" style="background-color: rgb(110 94 94 / 27%); border-top: rgb(51 43 43 / 9%);">
+  FOOD AND CIVIL SUPPLIES
+  </div>
+</div>
+</div>
+</div>
+<!-- End of Container -->
+
+    <!-- Footer -->
+    <div class="bg-dark text-secondary px-4 py-5 text-center">
+        <div class="py-5">
+            <h1 class="display-5 fw-bold text-white">Food & Civil Supplies</h1>
+            <div class="col-lg-6 mx-auto">
+                <p class="fs-5 mb-4">Department of Goa</p>
+                <hr>
+                <a href="#" class="fs-5 mb-4"><img src="https://img.icons8.com/nolan/64/instagram-new.png" /></a>
+                <a href="#" class="fs-5 mb-4" style="margin-right: 20px; margin-left: 20px;"><img
+                        src="https://img.icons8.com/nolan/64/twitter.png" /></a>
+                <a href="#" class="fs-5 mb-4"><img src="https://img.icons8.com/nolan/64/whatsapp.png" /></a>
+            </div>
+        </div>
+    </div>
+    <!-- End of Footer -->
 
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
-</body>
-
+  </body>
 </html>
