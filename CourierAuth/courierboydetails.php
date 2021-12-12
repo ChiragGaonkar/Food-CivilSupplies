42046d479024434fb4732dbc74352b1f0a746086
Akshad Tulaskar <95163463+Akshad05@users.noreply.github.com>
2021-12-12 17:50:20 +0530

    Updated

--- CourierAuth/courierboydetails.php
+++ CourierAuth/courierboydetails.php
@@ -8,6 +8,17 @@ if($_SERVER['REQUEST_METHOD']=='POST')
 $table="courier_data";
 $sql="select * from $table where CID=$ID";
 $display=mysqli_query($connection,$sql);
+
+$num=mysqli_num_rows($display);
+
+if($num==0)
+{
+    die("<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+    <strong>Invalid Login or Password!!!</strong>
+    <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+    </div>");
+}
+
 $row=mysqli_fetch_assoc($display);
 
 ?>
