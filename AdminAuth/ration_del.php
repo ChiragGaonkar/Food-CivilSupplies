f07a779101da7abf0131ab2c0fb795dfb36f9ae6
Akash Kumar Singh <83223139+akashkumarSingh34@users.noreply.github.com>
2021-12-17 02:58:51 +0530

    Update ration_del.php

--- AdminAuth/ration_del.php
+++ AdminAuth/ration_del.php
@@ -17,7 +17,7 @@ if (isset($_GET["deleteid"])) {
     $district = $row['DISTRICT'];
     $img_upload_path = $row['IMAGE'];
 
-    $sql = "INSERT INTO discarded_data(PID,PNAME,QUANTITY,PTYPE,PRICE,MANU_DATE,EXP_DATE,DISTRICT,IMAGE) values ($id,'$pname',$quantity,'$ptype',$price,'$md','$ed','$district','$img_upload_path')";
+    $sql = "INSERT INTO `discarded_data` (PID,PNAME,QUANTITY,PTYPE,PRICE,MANU_DATE,EXP_DATE,DISTRICT,IMAGE) values ($id,'$pname',$quantity,'$ptype',$price,'$md','$ed','$district','$img_upload_path')";
     $result1 = mysqli_query($connection, $sql);
 
     $sql="delete from `product_data` where Pid=$id";
